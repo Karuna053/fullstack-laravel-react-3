e2cd6354261d47297d3f997dd969d0d34c0de319
Karuna053 <karunayauw@gmail.com>
2024-08-30 07:35:45 +0700

    Adding basic token authentication to the FE.

--- react/src/components/GuestLayout.tsx
+++ react/src/components/GuestLayout.tsx
@@ -1,6 +1,13 @@
-import {Outlet} from "react-router-dom"
+import {Outlet, Navigate} from "react-router-dom"
+import {useStateContext} from "../contexts/ContextProvider.jsx"
 
 export default function GuestLayout() {
+  const {token} = useStateContext()
+  debugger;
+  if (token) {
+    return <Navigate to="/users" />
+  }
+
   return (
     <>
       <div>
