87a75bbd399e16f7d54e1ebc667db8da0a5df6af
Karuna053 <karunayauw@gmail.com>
2024-08-30 07:24:27 +0700

    Prepping contextes for the API

--- react/src/router.tsx
+++ react/src/router.tsx
@@ -1,21 +1,44 @@
-import {createBrowserRouter} from "react-router-dom";
-import Login from "./views/Login.jsx";
-import Signup from "./views/Signup.jsx";
-import Users from "./views/Users.jsx";
-import NotFound from "./views/NotFound.jsx";
+import { createBrowserRouter, RouteObject, Navigate } from "react-router-dom";
+import Login from "./views/Login.tsx";
+import Signup from "./views/Signup.tsx";
+import Users from "./views/Users.tsx";
+import Dashboard from "./views/Dashboard.tsx";
+import NotFound from "./views/NotFound.tsx";
+import DefaultLayout from "./components/DefaultLayout.tsx";
+import GuestLayout from "./components/GuestLayout.tsx";
 
-const router = createBrowserRouter( [
+const router: RouteObject = createBrowserRouter([
   {
-    path: '/login',
-    element: <Login />
+    path: '/',
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to="/users" />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      },
+      {
+        path: '/users',
+        element: <Users />
+      },
+    ]
   },
   {
-    path: '/signup',
-    element: <Signup />
-  },
-  {
-    path: '/users',
-    element: <Users />
+    path: '/',
+    element: <GuestLayout />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/signup',
+        element: <Signup />
+      },
+    ]
   },
   {
     path: '*',
