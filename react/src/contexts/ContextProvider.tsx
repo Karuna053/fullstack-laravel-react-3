f05dde56c6c72ffa430b55d98665914b3428d677
Karuna053 <karunayauw@gmail.com>
2024-08-30 07:58:31 +0700

    Basic register page

--- react/src/contexts/ContextProvider.tsx
+++ react/src/contexts/ContextProvider.tsx
@@ -12,7 +12,7 @@ export const ContextProvider = ({children}) => {
     name: 'John'
   });
   // const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
-  const [token, _setToken] = useState(123);
+  const [token, _setToken] = useState(null);
 
   const setToken = (token) => {
     _setToken(token)
--- react/src/views/Signup.tsx
+++ react/src/views/Signup.tsx
@@ -1,9 +1,31 @@
 import './../App.css'
+import {Link} from "react-router-dom"
 
 export default function Signup() {
+
+  const onSubmit = (ev) => {
+    ev.preventDefault()
+  }
+
   return (
     <>
-      Signup
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">
+            Signup for free
+          </h1>
+          <input type="name" placeholder="Full Name"/>
+          <input type="email" placeholder="Email Address"/>
+          <input type="password" placeholder="Password"/>
+          <input type="password" placeholder="Password Confirmation"/>
+          <button className="btn btn-block">Signup</button>
+          <p className="message">
+            Already Registered? <Link to="/login">Sign in</Link>
+          </p>
+        </form>
+      </div>
+    </div>
     </>
   )
 }
