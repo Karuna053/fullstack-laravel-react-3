e2f28456b46672e8caf3cfc7ba8b1e98e1d351bd
Karuna053 <karunayauw@gmail.com>
2024-08-30 13:12:08 +0700

    Making error handling more responsive from FE side

--- react/src/views/Signup.tsx
+++ react/src/views/Signup.tsx
@@ -1,6 +1,6 @@
 import './../App.css'
 import {Link} from "react-router-dom"
-import {useRef} from "react"
+import {useRef,useState} from "react"
 import axiosClient from "../axios-client.js"
 import {useStateContext} from "../contexts/ContextProvider.tsx";
 
@@ -9,6 +9,7 @@ export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmationRef = useRef();
+  const [errors, setErrors] = useState(null);
 
   const {setUser, setToken} = useStateContext()
 
@@ -32,6 +33,7 @@ export default function Signup() {
         const response = err.response;
         if (response && response.status == 422 ){ // Validation Error
          console.log(response.data.errors)
+         setErrors(response.data.errors);
         }
       })
   }
@@ -44,6 +46,14 @@ export default function Signup() {
             <h1 className="title">
               Signup for free
             </h1>
+
+            {errors && <div className="alert">
+              {Object.keys(errors).map(key => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+            }
+
             <input ref={nameRef} placeholder="Full Name"/>
             <input ref={emailRef} type="email" placeholder="Email Address"/>
             <input ref={passwordRef} type="password" placeholder="Password"/>
