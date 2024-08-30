fa7009f9669b5fe4a2cd4618d3236f7abba34ddc
Karuna053 <karunayauw@gmail.com>
2024-08-30 13:04:59 +0700

    Created the logic to sign user in via the react js application.

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -5,20 +5,27 @@ const axiosClient = axios.create({
 });
 
 axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.get('ACCESS_TOKEN')
+  const token = localStorage.getItem('ACCESS_TOKEN')
   config.headers.Authorization = `Bearer ${token}`
   return config;
 })
 
-axiosClient.interceptors.response.use((response) => {
-  return response
-}, (error) => {
-  const {response} = error;
-  if (response.status == 401) {
-    localStorage.removeItem('ACCESS_TOKEN')
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    try {
+      const { response: errorResponse } = error;
+      if (errorResponse && errorResponse.status === 401) {
+        localStorage.removeItem('ACCESS_TOKEN');
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  
+    throw error;
   }
-
-  throw error;
-})
+);
 
 export default axiosClient;
