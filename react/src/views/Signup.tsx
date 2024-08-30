f1bee191b4da38df3a4b8875d204bf11b9ae6979
Karuna053 <karunayauw@gmail.com>
2024-08-30 08:04:11 +0700

    Installing axios + code maintenance

--- react/src/views/Signup.tsx
+++ react/src/views/Signup.tsx
@@ -9,23 +9,23 @@ export default function Signup() {
 
   return (
     <>
-    <div className="login-signup-form animated fadeInDown">
-      <div className="form">
-        <form onSubmit={onSubmit}>
-          <h1 className="title">
-            Signup for free
-          </h1>
-          <input type="name" placeholder="Full Name"/>
-          <input type="email" placeholder="Email Address"/>
-          <input type="password" placeholder="Password"/>
-          <input type="password" placeholder="Password Confirmation"/>
-          <button className="btn btn-block">Signup</button>
-          <p className="message">
-            Already Registered? <Link to="/login">Sign in</Link>
-          </p>
-        </form>
+      <div className="login-signup-form animated fadeInDown">
+        <div className="form">
+          <form onSubmit={onSubmit}>
+            <h1 className="title">
+              Signup for free
+            </h1>
+            <input type="name" placeholder="Full Name"/>
+            <input type="email" placeholder="Email Address"/>
+            <input type="password" placeholder="Password"/>
+            <input type="password" placeholder="Password Confirmation"/>
+            <button className="btn btn-block">Signup</button>
+            <p className="message">
+              Already Registered? <Link to="/login">Sign in</Link>
+            </p>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   )
 }
